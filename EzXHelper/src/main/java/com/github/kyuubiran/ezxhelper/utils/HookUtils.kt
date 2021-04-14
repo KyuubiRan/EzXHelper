641b931e6e8ca4c1294b5c903bfc409c5cba138c
KyuubiRan <KyuubiRan@qq.com>
2021-04-14 10:16:56 +0800

    Bug fix: Changed replace hook return type Unit ->Any

--- EzXHelper/src/main/java/com/github/kyuubiran/ezxhelper/utils/HookUtils.kt
+++ EzXHelper/src/main/java/com/github/kyuubiran/ezxhelper/utils/HookUtils.kt
@@ -61,7 +61,7 @@ fun Method.hookAfter(
  */
 fun Method.replaceHook(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (XC_MethodHook.MethodHookParam) -> Unit
+    hook: (XC_MethodHook.MethodHookParam) -> Any
 ) {
     this.hookMethod(object : XC_MethodReplacement(priority) {
         override fun replaceHookedMethod(param: MethodHookParam): Any {
