e0e03853200e5e211dc11a1d24623634c1e14f16
KyuubiRan <KyuubiRan@qq.com>
2021-06-17 17:36:43 +0800

    Add multiple hook

--- EzXHelper/src/main/java/com/github/kyuubiran/ezxhelper/utils/HookUtils.kt
+++ EzXHelper/src/main/java/com/github/kyuubiran/ezxhelper/utils/HookUtils.kt
@@ -7,6 +7,9 @@ import de.robv.android.xposed.callbacks.XCallback
 import java.lang.reflect.Constructor
 import java.lang.reflect.Method
 
+typealias Hooker = (param: XC_MethodHook.MethodHookParam) -> Unit
+typealias ReplaceHooker = (param: XC_MethodHook.MethodHookParam) -> Any?
+
 /**
  * 扩展函数 hook方法/构造
  * @param hookCallback XC_MethodHook
@@ -26,7 +29,7 @@ fun Constructor<*>.hookMethod(hookCallback: XC_MethodHook) {
  */
 fun Method.hookBefore(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Unit
+    hook: Hooker
 ) {
     this.hookMethod(object : XC_MethodHook(priority) {
         override fun beforeHookedMethod(param: MethodHookParam) {
@@ -37,6 +40,20 @@ fun Method.hookBefore(
             }
         }
     })
+}
+
+/**
+ * 扩展函数 hook多个方法执行前
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Method>.hookBefore(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: Hooker
+) {
+    this.forEach {
+        it.hookBefore(priority, hooker)
+    }
 }
 
 /**
@@ -46,7 +63,7 @@ fun Method.hookBefore(
  */
 fun Constructor<*>.hookBefore(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Unit
+    hook: Hooker
 ) {
     this.hookMethod(object : XC_MethodHook(priority) {
         override fun beforeHookedMethod(param: MethodHookParam) {
@@ -60,13 +77,28 @@ fun Constructor<*>.hookBefore(
 }
 
 /**
+ * 扩展函数 hook多个构造执行前
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Constructor<*>>.hookBefore(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: Hooker
+) {
+    this.forEach {
+        it.hookBefore(priority, hooker)
+    }
+}
+
+
+/**
  * 扩展函数 hook方法执行后
  * @param priority 优先级 默认50
  * @param hook hook具体实现
  */
 fun Method.hookAfter(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Unit
+    hook: Hooker
 ) {
     this.hookMethod(object : XC_MethodHook(priority) {
         override fun afterHookedMethod(param: MethodHookParam) {
@@ -80,23 +112,51 @@ fun Method.hookAfter(
 }
 
 /**
+ * 扩展函数 hook多个方法执行后
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Method>.hookAfter(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: Hooker
+) {
+    this.forEach {
+        it.hookAfter(priority, hooker)
+    }
+}
+
+/**
  * 扩展函数 hook构造执行后
  * @param priority 优先级 默认50
- * @param hook hook具体实现
+ * @param hooker hook具体实现
  */
 fun Constructor<*>.hookAfter(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Unit
+    hooker: Hooker
 ) {
     this.hookMethod(object : XC_MethodHook(priority) {
         override fun afterHookedMethod(param: MethodHookParam) {
             try {
-                hook(param)
+                hooker(param)
             } catch (thr: Throwable) {
                 Log.t(thr)
             }
         }
     })
+}
+
+/**
+ * 扩展函数 hook多个构造执行后
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Constructor<*>>.hookAfter(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: Hooker
+) {
+    this.forEach {
+        it.hookAfter(priority, hooker)
+    }
 }
 
 /**
@@ -106,7 +166,7 @@ fun Constructor<*>.hookAfter(
  */
 fun Method.hookReplace(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Any?
+    hook: ReplaceHooker
 ) {
     this.hookMethod(object : XC_MethodReplacement(priority) {
         override fun replaceHookedMethod(param: MethodHookParam): Any? {
@@ -120,21 +180,53 @@ fun Method.hookReplace(
 }
 
 /**
+ * 扩展函数 替换多个方法
+ *
+ * 注意:会忽略hooker的返回值
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Method>.hookReplace(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: ReplaceHooker
+) {
+    this.forEach {
+        it.hookReplace(priority, hooker)
+    }
+}
+
+/**
  * 扩展函数 替换构造
  * @param priority 优先级 默认50
- * @param hook hook具体实现
+ * @param hooker hook具体实现
  */
 fun Constructor<*>.hookReplace(
     priority: Int = XCallback.PRIORITY_DEFAULT,
-    hook: (param: XC_MethodHook.MethodHookParam) -> Any?
+    hooker: ReplaceHooker
 ) {
     this.hookMethod(object : XC_MethodReplacement(priority) {
         override fun replaceHookedMethod(param: MethodHookParam): Any? {
             return try {
-                hook(param)
+                hooker(param)
             } catch (thr: Throwable) {
                 Log.t(thr)
             }
         }
     })
+}
+
+/**
+ * 扩展函数 替换多个构造
+ *
+ * 注意:会忽略hooker的返回值
+ * @param priority 优先级 默认50
+ * @param hooker hook具体实现
+ */
+fun Array<Constructor<*>>.hookReplace(
+    priority: Int = XCallback.PRIORITY_DEFAULT,
+    hooker: ReplaceHooker
+) {
+    this.forEach {
+        it.hookReplace(priority, hooker)
+    }
 }
