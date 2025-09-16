a12960491b9ebc265f7620d8ecf86c57d7e534ef
Irena <140869597+re-zero001@users.noreply.github.com>
2025-09-16 18:50:04 +0800

    Implemented Warpper

--- xposed-api-100/src/main/java/io/github/kyuubiran/ezxhelper/xposed/interfaces/IMethodHookCallback.kt
+++ xposed-api-100/src/main/java/io/github/kyuubiran/ezxhelper/xposed/interfaces/IMethodHookCallback.kt
@@ -1,11 +1,79 @@
 package io.github.kyuubiran.ezxhelper.xposed.interfaces
 
 import io.github.libxposed.api.XposedInterface
+import java.lang.reflect.Member
+
+class BeforeHookParam(private val original: XposedInterface.BeforeHookCallback) {
+    val member: Member
+        get() = original.member
+
+    val thisObject: Any
+        get() = original.thisObject
+            ?: throw IllegalStateException("Expected non-null thisObject but got null")
+
+    val thisObjectOrNull: Any?
+        get() = original.thisObject
+
+    val args: Array<Any>
+        get() = original.args.map {
+            it ?: throw IllegalStateException("Expected non-null argument but got null in args array")
+        }.toTypedArray()
+
+    val argsOrNull: Array<Any?>
+        get() = original.args
+
+    var result: Any?
+        get() = null
+        set(value) {
+            original.returnAndSkip(value)
+        }
+
+    var throwable: Throwable?
+        get() = null
+        set(value) {
+            original.throwAndSkip(value)
+        }
+}
+
+class AfterHookParam(private val original: XposedInterface.AfterHookCallback) {
+    val member: Member
+        get() = original.member
+
+    val thisObject: Any
+        get() = original.thisObject
+            ?: throw IllegalStateException("Expected non-null thisObject but got null")
+
+    val thisObjectOrNull: Any?
+        get() = original.thisObject
+
+    val args: Array<Any>
+        get() = original.args.map {
+            it ?: throw IllegalStateException("Expected non-null argument but got null in args array")
+        }.toTypedArray()
+
+    val argsOrNull: Array<Any?>
+        get() = original.args
+
+    val isSkipped: Boolean
+        get() = original.isSkipped
+
+    var result: Any?
+        get() = original.result
+        set(value) {
+            original.setResult(value)
+        }
+
+    var throwable: Throwable?
+        get() = original.throwable
+        set(value) {
+            original.throwable = value
+        }
+}
 
 fun interface IMethodBeforeHookCallback {
-    fun onMethodHooked(param: XposedInterface.BeforeHookCallback)
+    fun onMethodHooked(param: BeforeHookParam)
 }
 
 fun interface IMethodAfterHookCallback {
-    fun onMethodHooked(param: XposedInterface.AfterHookCallback)
+    fun onMethodHooked(param: AfterHookParam)
 }
